7f30ca867e7259903715ad7eb67b150a3e8d3fa1
Simon Renoult <contact@simonrenoult.me>
2021-12-22 18:28:50 +0100

    chore: autorise les imports natifs nodejs

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'prettier'
   ],
   rules: {
+    '@typescript-eslint/no-var-requires': 0,
     '@typescript-eslint/no-empty-interface': 0,
     '@typescript-eslint/no-unused-vars': [
       "warn",
