39e3f64a08e114f8359e28fd9a49ce6cd71c7845
Simon Renoult <contact@simonrenoult.me>
2022-03-23 11:10:17 +0100

    chore: suppression d'un .only qui traine

--- src/modules/catalogue-de-formations/test/write/infrastructure/catalogue-de-formation-sql.test.ts
+++ src/modules/catalogue-de-formations/test/write/infrastructure/catalogue-de-formation-sql.test.ts
@@ -5,7 +5,7 @@ import { Fixtures } from "../../fixtures";
 import { initialiserLaBaseDeDonneesDeTest } from "../../utils";
 import { CodeDeFormation } from "../../../write/domain/code-de-formation";
 
-describe.only(CatalogueDeFormationsSql.name, () => {
+describe(CatalogueDeFormationsSql.name, () => {
   it("permet de lire un agrégat après avoir été persisté", async () => {
     // Given
     const client = await initialiserLaBaseDeDonneesDeTest();
