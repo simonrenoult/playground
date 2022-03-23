6bf78c3d37ad770cea5d1c282f72494d102d4f36
Simon Renoult <contact@simonrenoult.me>
2022-03-23 15:33:46 +0100

    chore(build): ajout du host pour heroku

--- src/bin/start.ts
+++ src/bin/start.ts
@@ -1,10 +1,11 @@
 import serveur from "../infrastructure/serveur";
 
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT, 10) || 3000;
+const HOST = process.env.HOST || "0.0.0.0";
 
 async function start(): Promise<void> {
   try {
-    await serveur.listen(PORT);
+    await serveur.listen({ port: PORT, host: HOST });
   } catch (err) {
     serveur.log.error(err);
     process.exit(1);
